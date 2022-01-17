6813a5e41c7411dfbba6a7479ba60a5670a31cf6
Vladislav <vlad120686@gmail.com>
2022-01-18 01:31:02 +0200

    without hooks

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
+import React from 'react';
 import './App.css';
-import React, { useEffect, useState } from 'react';
 import Datatimer from './Component/Datatimer';
 
 function App() {
-    const [timerDays, setTimerDays] = useState();
-    const [timerHours, setTimerHours] = useState();
-    const [timerMinutes, setTimerMinutes] = useState();
-    const [timerSeconds, setTimerSeconds] = useState();
+    const countDownDate = new Date('June 1,2022').getTime();
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    let objTime;
 
-    let interval;
+    const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+    const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)) + 1;
+    const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+    const seconds = Math.floor((distance % (60 * 1000)) / 1000);
 
-    const startTimer = () => {
-        const countDownDate = new Date('June 1,2022').getTime();
-
-        interval = setInterval(() => {
-            const now = new Date().getTime();
-
-            const distance = countDownDate - now;
-
-            const days = Math.floor(distance / (24 * 60 * 60 * 1000));
-            const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)) + 1;
-            const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
-            const seconds = Math.floor((distance % (60 * 1000)) / 1000);
-
-            if (distance < 0) {
-                clearInterval(interval.current);
-            } else {
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-        });
-    };
-
-    useEffect(() => {
-        startTimer();
-    });
+    if (distance < 0) {
+        clearInterval();
+    } else {
+        objTime = {
+            day: days,
+            hour: hours,
+            minute: minutes,
+            second: seconds,
+        };
+    }
 
     return (
         <div className="App">
-            <Datatimer timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSeconds={timerSeconds} />
+            <Datatimer {...objTime} />
         </div>
     );
 }
