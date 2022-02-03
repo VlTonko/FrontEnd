49dda9350856981e7064d6d462d2f3a3809380e6
user <vlad120686@gmail.com>
2022-02-03 13:22:54 +0200

    tests

--- src/components/todoitems/__tests__/TodoItem.test.js
+++ src/components/todoitems/__tests__/TodoItem.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {mount, shallow} from 'enzyme';
-import TodoItem from "./components/todoitems/TodoItem";
+import TodoItem from "../TodoItem";
 
 describe('TodoItem', () => {
     let props;
